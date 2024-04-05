54c459b0afa7be2ef7c03692dc97b064105754f4
Samir Isagov <SAISO@vestas.com>
2024-04-05 14:23:16 +0200

    Test the navigating to page

---
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using(IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:44108/");
+            }
+        }
+    }
+}
